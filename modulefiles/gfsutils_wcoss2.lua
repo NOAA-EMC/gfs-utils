b4b27199142ea2ff1a6f4e15ad92735737583fa8
Rahul Mahajan <aerorahul@users.noreply.github.com>
2022-10-05 17:01:25 -0400

    gempak causes some issues on wcoss2 with netcdf

--- modulefiles/gfsutils_wcoss2.lua
+++ modulefiles/gfsutils_wcoss2.lua
@@ -1,33 +1,30 @@
 help([[
-Build environment for workflow utilities on WCOSS2
+Build environment for GFS utilities on WCOSS2
 ]])
 
-load(pathJoin("PrgEnv-intel", "8.1.0"))
-load(pathJoin("craype", "2.7.10"))
-load(pathJoin("intel", "19.1.3.304"))
-load(pathJoin("cray-mpich", "8.1.9"))
+local PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
+local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
+local craype_ver=os.getenv("craype_ver") or "2.7.10"
+local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
+local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
 
-load(pathJoin("cmake", "3.20.2"))
+load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
+load(pathJoin("intel", intel_ver))
+load(pathJoin("craype", craype_ver))
+load(pathJoin("cray-mpich", cray_mpich_ver))
+load(pathJoin("cmake", cmake_ver))
 
 load(pathJoin("jasper", "2.0.25"))
 load(pathJoin("zlib", "1.2.11"))
 load(pathJoin("libpng", "1.6.37"))
 
-load(pathJoin("hdf5", "1.10.6"))
-load(pathJoin("netcdf", "4.7.4"))
+load("gfsutils_common")
+unload("ncio")
 
-load(pathJoin("bacio", "2.4.1"))
-load(pathJoin("g2", "3.4.5"))
-load(pathJoin("ip", "3.3.3"))
-load(pathJoin("nemsio", "2.5.2"))
-load(pathJoin("sp", "2.3.3"))
-load(pathJoin("w3emc", "2.9.2"))
-load(pathJoin("w3nco", "2.4.1"))
-load(pathJoin("nemsiogfs", "2.5.3"))
-load(pathJoin("ncio", "1.0.0"))
-load(pathJoin("landsfcutil", "2.4.1"))
-load(pathJoin("sigio", "2.3.2"))
-load(pathJoin("bufr", "11.5.0"))
-load(pathJoin("gempak", "7.14.1"))
+pushenv("HPC_OPT", "/apps/ops/para/libs")
+prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/compiler/intel/19.1.3.304")
+prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.7")
 
-load(pathJoin("wgrib2", "2.0.8"))
+load("ncio/1.1.2")
+
+whatis("Description: GFS utilities environment on WCOSS2 with Intel Compilers")
