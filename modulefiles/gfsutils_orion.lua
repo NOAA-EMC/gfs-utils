bed36b4a074cc0a2253891c3830e0ed04cc6e654
kate.friedman <kate.friedman@noaa.gov>
2022-10-25 13:09:44 -0500

    Update modulepath and hpc module for Orion
    - Use stack install copy that Hang Lei manages.
    - Move to hpc/1.2.0 at direction of Hang Lei, so g2/3.4.5 can be accessed.
    
    Refs #14

--- modulefiles/gfsutils_orion.lua
+++ modulefiles/gfsutils_orion.lua
@@ -2,9 +2,9 @@ help([[
 Build environment for GFS utilities on Orion
 ]])
 
-prepend_path("MODULEPATH", "/apps/contrib/NCEP/libs/hpc-stack/modulefiles/stack")
+prepend_path("MODULEPATH", "/apps/contrib/NCEP/hpc-stack/libs/hpc-stack/modulefiles/stack")
 
-local hpc_ver=os.getenv("hpc_ver") or "1.1.0"
+local hpc_ver=os.getenv("hpc_ver") or "1.2.0"
 local hpc_intel_ver=os.getenv("hpc_intel_ver") or "2018.4"
 local hpc_impi_ver=os.getenv("hpc_impi_ver") or "2018.4"
 local cmake_ver=os.getenv("cmake_ver") or "3.22.1"
