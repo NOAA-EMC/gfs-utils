5b159a9e4b92e38db4af2c56b93d7fb68ea32a8b
DavidHuber <David.Huber@noaa.gov>
2023-09-20 15:04:31 +0000

    Upgrade to spack-stack libraries on Hera #23

--- modulefiles/gfsutils_common.lua
+++ modulefiles/gfsutils_common.lua
@@ -2,7 +2,8 @@ help([[
 Load common modules to build GFS utilities on all machines
 ]])
 
-local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
+local netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2"
+local netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.0"
 
 local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
@@ -18,8 +19,12 @@ local g2_ver=os.getenv("g2_ver") or "3.4.5"
 local landsfcutil_ver=os.getenv("landsfcutil_ver") or "2.4.1"
 local gempak_ver=os.getenv("gempak_ver") or "7.14.1"
 local wgrib2_ver=os.getenv("wgrib2_ver") or "2.0.8"
+local libpng_ver=os.getenv("libpng_ver") or "1.6.37"
 
-load(pathJoin("netcdf", netcdf_ver))
+load(pathJoin("libpng", libpng_ver))
+
+load(pathJoin("netcdf-c", netcdf_c_ver))
+load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
 
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
@@ -29,7 +34,7 @@ load(pathJoin("ip", ip_ver))
 load(pathJoin("sigio", sigio_ver))
 load(pathJoin("sfcio", sfcio_ver))
 load(pathJoin("nemsio", nemsio_ver))
-load(pathJoin("wrf_io", wrf_io_ver))
+load(pathJoin("wrf-io", wrf_io_ver))
 load(pathJoin("ncio", ncio_ver))
 load(pathJoin("g2", g2_ver))
 load(pathJoin("landsfcutil", landsfcutil_ver))
