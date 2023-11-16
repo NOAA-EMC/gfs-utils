320358afe3f7407367027e2d2c45d703d7f134c3
DavidHuber <david.huber@noaa.gov>
2023-11-16 20:30:48 +0000

    Fix Orion intel version. #23

--- modulefiles/gfsutils_orion.lua
+++ modulefiles/gfsutils_orion.lua
@@ -4,7 +4,7 @@ Build environment for GFS utilities on Orion
 
 prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2022.0.2"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
