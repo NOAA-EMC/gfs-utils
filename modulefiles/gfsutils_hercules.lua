1bdc3de6f37825ad5872f52d3675795cb8fe3811
David Huber <david.huber@noaa.gov>
2024-01-25 07:44:35 -0600

    Load wgrib2/2.0.8 on Hercules.

--- modulefiles/gfsutils_hercules.lua
+++ modulefiles/gfsutils_hercules.lua
@@ -11,7 +11,6 @@ local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-setenv("wgrib2_ver", "3.1.1")
 load(pathJoin("cmake", cmake_ver))
 
 load("gfsutils_common")
