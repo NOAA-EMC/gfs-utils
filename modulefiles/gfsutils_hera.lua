d574551ff74911eb746cf9d2f010f83e5a2d6fc5
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2024-01-25 17:37:38 +0000

    Generic module file for RDHPCS Hera.

---
+++ modulefiles/gfsutils_hera.lua
@@ -0,0 +1,17 @@
+help([[
+Build environment for GFS utilities on Hera
+]])
+
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
+
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("cmake", cmake_ver))
+
+load("gfsutils_common")
+
+whatis("Description: GFS utilities environment on Hera with Intel Compilers")
