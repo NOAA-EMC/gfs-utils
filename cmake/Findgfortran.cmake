3287fbac779726d746bf3d0e4cceafaeeb4c0c0d
Rahul Mahajan <aerorahul@users.noreply.github.com>
2024-03-10 11:51:05 -0400

    make rdbfmsua.fd optional

--- cmake/Findgfortran.cmake
+++ cmake/Findgfortran.cmake
@@ -1,7 +1,9 @@
 find_library(
   GFORTRAN_LIBRARY
   NAMES libgfortran.a
-  HINTS /apps/gnu/gcc-6.5.0
+  HINTS ${GFORTRAN_LIBRARY_DIRS}
+        /apps/gnu/gcc-6.5.0
+        /opt/cray/pe/gcc/10.3.0/snos
   PATH_SUFFIXES lib lib64
   DOC "Path to GFORTRAN_LIBRARY"
   )
@@ -11,7 +13,7 @@ mark_as_advanced(GFORTRAN_LIBRARY)
 message(DEBUG "[Findgfortran.cmake]: creating target gfortran::gfortran")
 add_library(gfortran::gfortran UNKNOWN IMPORTED)
 
-message(STATUS "GFORTRAN_LIBRARY = ${GFORTRAN_LIBRARY}")
+message(DEBUG "GFORTRAN_LIBRARY = ${GFORTRAN_LIBRARY}")
 
 message(DEBUG "[Findgfortran.cmake]: linking with gfortran.a")
 set_target_properties(gfortran::gfortran PROPERTIES IMPORTED_LOCATION ${GFORTRAN_LIBRARY})
