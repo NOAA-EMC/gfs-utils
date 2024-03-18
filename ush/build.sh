192b7d6c3626eb674ceb9fee972d1e77ee55dfa5
Rahul Mahajan <aerorahul@users.noreply.github.com>
2024-03-18 17:47:31 -0400

    shellcheck it

--- ush/build.sh
+++ ush/build.sh
@@ -3,6 +3,7 @@
 set -eu
 
 # Get the root of the cloned GFS-utils directory
+#shellcheck disable=SC2155
 readonly DIR_ROOT=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.." && pwd -P)
 
 # User Options
@@ -15,45 +16,45 @@ INSTALL_PREFIX=${INSTALL_PREFIX:-"${DIR_ROOT}/install"}
 #==============================================================================#
 
 # Detect machine (sets MACHINE_ID)
-source $DIR_ROOT/ush/detect_machine.sh
+source "${DIR_ROOT}/ush/detect_machine.sh"
 
 # Load modules
-source $DIR_ROOT/ush/module-setup.sh
-module use $DIR_ROOT/modulefiles
-module load gfsutils_$MACHINE_ID.$COMPILER
+source "${DIR_ROOT}/ush/module-setup.sh"
+module use "${DIR_ROOT}/modulefiles"
+module load "gfsutils_${MACHINE_ID}.${COMPILER}"
 module list
 
 # Collect BUILD Options
-CMAKE_OPTS+=" -DCMAKE_BUILD_TYPE=$BUILD_TYPE"
+CMAKE_OPTS+=" -DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
 
 # Install destination for built executables, libraries, CMake Package config
-CMAKE_OPTS+=" -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX"
+CMAKE_OPTS+=" -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}"
 
 # Re-use or create a new BUILD_DIR (Default: create new BUILD_DIR)
-[[ ${BUILD_CLEAN:-"YES"} =~ [yYtT] ]] && rm -rf $BUILD_DIR
-mkdir -p $BUILD_DIR && cd $BUILD_DIR
+[[ ${BUILD_CLEAN:-"YES"} =~ [yYtT] ]] && rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}" && cd "${BUILD_DIR}"
 
 # Configure, build, install
 set -x
-cmake $CMAKE_OPTS $DIR_ROOT
-make -j ${BUILD_JOBS:-8} VERBOSE=${BUILD_VERBOSE:-}
+cmake "${CMAKE_OPTS}" "${DIR_ROOT}"
+make -j "${BUILD_JOBS:-8}" VERBOSE="${BUILD_VERBOSE:-}"
 make install
 set +x
 
-# Build rdbfmsua.x separately due to GEMPAK issues
+# Build rdbfmsua.x with makefile separately due to GEMPAK issues in cmake
 # Load modules
-source $DIR_ROOT/ush/module-setup.sh
-module use $DIR_ROOT/modulefiles
-module load rdbfmsua_$MACHINE_ID.$COMPILER
+source "${DIR_ROOT}/ush/module-setup.sh"
+module use "${DIR_ROOT}/modulefiles"
+module load "rdbfmsua_${MACHINE_ID}.${COMPILER}"
 module show gempak
 module list
 
-cd $DIR_ROOT/src/rdbfmsua.fd
+cd "${DIR_ROOT}/src/rdbfmsua.fd"
 export MACHINE_ID
 export COMPILER
-export PREFIX=${INSTALL_PREFIX}/bin
+export PREFIX="${INSTALL_PREFIX}"
 make clean
-make VERBOSE=${BUILD_VERBOSE:-}
-make install VERBOSE=${BUILD_VERBOSE:-}
+make VERBOSE="${BUILD_VERBOSE:-}"
+make install VERBOSE="${BUILD_VERBOSE:-}"
 
 exit
