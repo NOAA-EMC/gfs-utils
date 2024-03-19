57492f2805e7c4275bf216b0fd321bccc99a22e1
Rahul Mahajan <aerorahul@users.noreply.github.com>
2024-03-19 16:29:32 -0400

    Update modulefiles/rdbfmsua_wcoss2.intel.lua

--- modulefiles/rdbfmsua_wcoss2.intel.lua
+++ modulefiles/rdbfmsua_wcoss2.intel.lua
@@ -7,10 +7,6 @@ local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
 local craype_ver=os.getenv("craype_ver") or "2.7.10"
 local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
 
-local jasper_ver=os.getenv("jasper_ver") or "2.0.25"
-local zlib_ver=os.getenv("zlib_ver") or "1.2.11"
-local libpng_ver=os.getenv("libpng_ver") or "1.6.37"
-local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
 
 local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
