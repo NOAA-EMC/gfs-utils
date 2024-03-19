4f7ab89b48e1fc690dfbe541519bb4a131d20d75
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2024-03-19 21:22:07 +0000

    Remove unneeded mpich module

--- modulefiles/rdbfmsua_wcoss2.intel.lua
+++ modulefiles/rdbfmsua_wcoss2.intel.lua
@@ -5,8 +5,6 @@ Build environment for GFS utilities on WCOSS2
 local PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
 local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
 local craype_ver=os.getenv("craype_ver") or "2.7.10"
-local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
-
 
 local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
@@ -16,7 +14,6 @@ local gempak_ver=os.getenv("gempak_ver") or "7.14.1"
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
 load(pathJoin("craype", craype_ver))
-load(pathJoin("cray-mpich", cray_mpich_ver))
 
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
