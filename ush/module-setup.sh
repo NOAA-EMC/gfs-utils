719ebc6db816a973d7d149d477ca7cc2d19601c8
David Burrows <David.Burrows@noaa.gov>
2024-04-19 10:31:32 -0400

    update ush/module-setup.sh to align with ufs_model's module-setup script

--- ush/module-setup.sh
+++ ush/module-setup.sh
@@ -61,6 +61,14 @@ elif [[ $MACHINE_ID = stampede* ]] ; then
 
 elif [[ $MACHINE_ID = gaea* ]] ; then
     # We are on GAEA.
+    if ( ! eval module help > /dev/null 2>&1 ) ; then
+        # We cannot simply load the module command.  The GAEA
+        # /etc/profile modifies a number of module-related variables
+        # before loading the module command.  Without those variables,
+        # the module command fails.  Hence we actually have to source
+        # /etc/profile here.
+        source /etc/profile
+    fi
     module reset
 
 elif [[ $MACHINE_ID = expanse* ]]; then
