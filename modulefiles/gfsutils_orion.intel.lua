7836e5578e6cdd281a90621bea4945ee044bc56b
Rahul Mahajan <aerorahul@users.noreply.github.com>
2024-06-28 11:05:06 -0400

    update modules on orion for rocky 8.  gempak is not available

--- modulefiles/gfsutils_orion.intel.lua
+++ modulefiles/gfsutils_orion.intel.lua
@@ -2,10 +2,15 @@ help([[
 Build environment for GFS utilities on Orion
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+-- Spack Stack installation specs
+local ss_dir="/work/noaa/epic/role-epic/spack-stack/orion"
+local ss_ver=os.getenv("stack_ver") or "1.6.0"
+local ss_env=os.getenv("stack_env") or "gsi-addon-env-rocky9"
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2022.0.2"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
+prepend_path("MODULEPATH", pathJoin(ss_dir, "spack-stack-" .. ss_ver, "envs", ss_env, "install/modulefiles/Core"))
+
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
 load(pathJoin("stack-intel", stack_intel_ver))
