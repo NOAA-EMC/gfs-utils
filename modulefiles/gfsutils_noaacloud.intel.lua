da0cc143f57a962a7a2141976037c34615c3f91a
Wei Huang <Wei.Huang@noaa.gov>
2024-10-18 15:44:46 +0000

    move local defines together, and keep load together to improve readibility

--- modulefiles/gfsutils_noaacloud.intel.lua
+++ modulefiles/gfsutils_noaacloud.intel.lua
@@ -5,14 +5,14 @@ Build environment for GFS utilities on NOAA Cloud
 prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/apps/modules/modulefiles")
 
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+
 load("gnu")
-stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
 load(pathJoin("stack-intel", stack_intel_ver))
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 unload("gnu")
-
-local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
 load(pathJoin("cmake", cmake_ver))
 
