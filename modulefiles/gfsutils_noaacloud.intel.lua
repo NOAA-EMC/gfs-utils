4848ecbb5e713b16127433e11f7d3edc6ac784c4
Wei Huang <wei.huang@noaa.gov>
2024-10-18 10:41:25 -0600

    Make gfs-utils compile on CSPs with Rocky 8 (#81)
    Support Rocky 8 on CSPs.

--- modulefiles/gfsutils_noaacloud.intel.lua
+++ modulefiles/gfsutils_noaacloud.intel.lua
@@ -2,15 +2,18 @@ help([[
 Build environment for GFS utilities on NOAA Cloud
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/apps/modules/modulefiles")
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.3.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.3.0"
-local cmake_ver=os.getenv("cmake_ver") or "3.20.1"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
+load("gnu")
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+unload("gnu")
+
 load(pathJoin("cmake", cmake_ver))
 
 load("gfsutils_common")
