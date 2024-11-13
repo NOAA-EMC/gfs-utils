22ae6c315e14390e1214fd058e26a6de2d2066ba
DavidBurrows-NCO <David.Burrows@noaa.gov>
2024-11-13 14:29:07 -0500

    Update C5 C6 modulefiles

--- modulefiles/gfsutils_gaeac5.intel.lua
+++ modulefiles/gfsutils_gaeac5.intel.lua
@@ -1,11 +1,10 @@
 help([[
   This module loads libraries required for building and running GFS UTILS 
-  on the NOAA RDHPC machine Gaea C5 using Intel-2023.1.0.
+  on the NOAA RDHPC machine Gaea C5 using Intel-2023.2.0.
 ]])
 
-whatis([===[Loads libraries needed for building the UFS Weather Model on Gaea C5]===])
+whatis([===[Loads libraries needed for building the GFS utilities on Gaea C5]===])
 
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
@@ -14,16 +13,12 @@ load(pathJoin("stack-intel", stack_intel_ver))
 stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.28"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
-stack_python_ver=os.getenv("stack_python_ver") or "3.11.6"
-load(pathJoin("stack-python", stack_python_ver))
-
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
 load("gfsutils_common")
 load("nccmp/1.9.0.1")
 
-unload("darshan-runtime")
 unload("cray-libsci")
 
 setenv("CC","cc")
--- modulefiles/gfsutils_gaeac6.intel.lua
+++ modulefiles/gfsutils_gaeac6.intel.lua
@@ -1,11 +1,10 @@
 help([[
   This module loads libraries required for building and running GFS UTILS 
-  on the NOAA RDHPC machine Gaea C6 using Intel-2023.1.0.
+  on the NOAA RDHPC machine Gaea C6 using Intel-2023.2.0.
 ]])
 
-whatis([===[Loads libraries needed for building the UFS Weather Model on Gaea C6 ]===])
+whatis([===[Loads libraries needed for building the GFS utilities on Gaea C6 ]===])
 
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.6.0/envs/gsi-addon/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
@@ -14,16 +13,12 @@ load(pathJoin("stack-intel", stack_intel_ver))
 stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.29"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
-stack_python_ver=os.getenv("stack_python_ver") or "3.11.6"
-load(pathJoin("stack-python", stack_python_ver))
-
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
 load("gfsutils_common")
 load("nccmp/1.9.0.1")
 
-unload("darshan-runtime")
 unload("cray-libsci")
 
 setenv("CC","cc")
