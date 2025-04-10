eb7a75258f64075a63429d2ae90c9c5d825bcb9c
Wei Huang <Wei.Huang@noaa.gov>
2025-04-10 20:16:08 +0000

    change ncio, landsfcutl module places

--- modulefiles/gfsutils_container.intel.lua
+++ modulefiles/gfsutils_container.intel.lua
@@ -3,6 +3,8 @@ Build environment for GFS utilities in a container
 ]])
 
 prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/intel-oneapi-mpi/2021.9.0/intel/2021.10.0")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/intel/2021.10.0")
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
@@ -11,9 +13,9 @@ local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load("gnu")
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-load(pathJoin("cmake", cmake_ver))
 unload("gnu")
 
+load(pathJoin("cmake", cmake_ver))
 load("gfsutils_common")
 
 whatis("Description: GFS utilities environment in container with Intel Compilers")
