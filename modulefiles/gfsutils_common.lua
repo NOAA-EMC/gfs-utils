184513dc068b07b47106de6dcf8a0b932dd69136
David Huber <david.huber@noaa.gov>
2025-05-28 16:03:03 -0400

    Remove sp module (moved to ip)

--- modulefiles/gfsutils_common.lua
+++ modulefiles/gfsutils_common.lua
@@ -8,7 +8,6 @@ local netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.1"
 local bufr_ver=os.getenv("bufr_ver") or "12.1.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
 local w3emc_ver=os.getenv("w3emc_ver") or "2.10.0"
-local sp_ver=os.getenv("sp_ver") or "2.5.0"
 local ip_ver=os.getenv("ip_ver") or "5.1.0"
 local sigio_ver=os.getenv("sigio_ver") or "2.3.3"
 local nemsio_ver=os.getenv("nemsio_ver") or "2.5.4"
@@ -30,7 +29,6 @@ load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
 load(pathJoin("w3emc", w3emc_ver))
-load(pathJoin("sp", sp_ver))
 load(pathJoin("ip", ip_ver))
 load(pathJoin("sigio", sigio_ver))
 load(pathJoin("sfcio", sfcio_ver))
