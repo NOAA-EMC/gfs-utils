775783972a1a653b29e1112b72ff2dcb735d2f96
Wei Huang <Wei.Huang@noaa.gov>
2025-06-13 16:35:25 +0000

    no need (cannot) load/unload gnu

--- modulefiles/gfsutils_container.intel.lua
+++ modulefiles/gfsutils_container.intel.lua
@@ -10,10 +10,8 @@ local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
--- load("gnu")
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
--- unload("gnu")
 
 load(pathJoin("cmake", cmake_ver))
 setenv("bufr_ver", "12.0.1")
