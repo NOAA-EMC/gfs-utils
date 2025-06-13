36553223250c5acdb3bf612d12c675cd1e236031
David Huber <david.huber@noaa.gov>
2025-06-13 11:27:41 -0400

    Removing unsupported platforms

--- ush/module-setup.sh
+++ ush/module-setup.sh
@@ -29,59 +29,21 @@ elif [[ $MACHINE_ID = wcoss2 ]]; then
     # We are on WCOSS2
     module reset
 
-elif [[ $MACHINE_ID = cheyenne* ]] ; then
-    # We are on NCAR Cheyenne
-    if ( ! eval module help > /dev/null 2>&1 ) ; then
-        source /glade/u/apps/ch/modulefiles/default/localinit/localinit.sh
-    fi
-    module purge
-
 elif [[ $MACHINE_ID = container ]] ; then
     # We are in a container
     if ( ! eval module help > /dev/null 2>&1 ) ; then
+        # shellcheck disable=1091
         source /usr/lmod/lmod/init/bash
-    fi
-    module purge
-
-elif [[ $MACHINE_ID = stampede* ]] ; then
-    # We are on TACC Stampede
-    if ( ! eval module help > /dev/null 2>&1 ) ; then
-        source /opt/apps/lmod/lmod/init/bash
     fi
     module purge
 
 elif [[ ${MACHINE_ID} = gaeac6 ]]; then
     # We are on GAEA C6.
     if ( ! eval module help > /dev/null 2>&1 ) ; then
+        # shellcheck disable=1091
         source /opt/cray/pe/lmod/lmod/init/bash
     fi
     module reset
-
-elif [[ $MACHINE_ID = gaeac5 ]] ; then
-    # We are on GAEA C5.
-    if ( ! eval module help > /dev/null 2>&1 ) ; then
-        # We cannot simply load the module command.  The GAEA
-        # /etc/profile modifies a number of module-related variables
-        # before loading the module command.  Without those variables,
-        # the module command fails.  Hence we actually have to source
-        # /etc/profile here.
-        source /etc/profile
-    fi
-    module reset
-
-elif [[ $MACHINE_ID = expanse* ]]; then
-    # We are on SDSC Expanse
-    if ( ! eval module help > /dev/null 2>&1 ) ; then
-        source /etc/profile.d/modules.sh
-    fi
-    module purge
-    module load slurm/expanse/20.02.3
-
-elif [[ $MACHINE_ID = discover* ]]; then
-    # We are on NCCS discover
-    export SPACK_ROOT=/discover/nobackup/mapotts1/spack
-    export PATH=$PATH:$SPACK_ROOT/bin
-    . $SPACK_ROOT/share/spack/setup-env.sh
 
 elif [[ ${MACHINE_ID} = noaacloud ]] ; then
     # We are on NOAA Cloud
