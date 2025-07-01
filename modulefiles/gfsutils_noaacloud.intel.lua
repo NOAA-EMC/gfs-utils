d570193d5be9070b25df0a6a7da9696541a61a6e
Taylor Roper <taylor.roper@noaa.gov>
2025-07-01 21:54:38 +0000

    Add gcc version and maintain gnu stack (#119)

--- modulefiles/gfsutils_noaacloud.intel.lua
+++ modulefiles/gfsutils_noaacloud.intel.lua
@@ -5,14 +5,14 @@ Build environment for GFS utilities on NOAA Cloud
 prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/apps/modules/modulefiles")
 
+local gcc_ver=os.getenv("gcc_ver") or "13.2.0"
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
-load("gnu")
+load(pathJoin("gnu", gcc_ver))
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-unload("gnu")
 
 load(pathJoin("cmake", cmake_ver))
 
