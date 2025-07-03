03e6971d0983d309f47b96be45a79e6e9333dd2d
David Huber <david.huber@noaa.gov>
2025-07-03 13:43:41 -0400

    Correct spack-stack paths for MSU and hera

--- modulefiles/gfsutils_hera.intel.lua
+++ modulefiles/gfsutils_hera.intel.lua
@@ -5,10 +5,10 @@ Build environment for GFS utilities on Hera
 prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
 local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or "2021.13"
 
 load(pathJoin("stack-oneapi", stack_oneapi_ver))
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
 
 load("gfsutils_common")
 
--- modulefiles/gfsutils_hercules.intel.lua
+++ modulefiles/gfsutils_hercules.intel.lua
@@ -4,11 +4,11 @@ Build environment for GFS utilities on Hercules
 
 prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
 
-local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.1.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+local stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or "2021.13"
 
-load(pathJoin("stack-oneapi", stack_intel_ver))
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
 
 load("gfsutils_common")
 
