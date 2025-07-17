bd47604fcd0d43ac44abeb2710072276a197fb4e
David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>
2025-07-17 08:38:52 -0400

    Update modulefiles/rdbfmsua_orion.intel.lua

--- modulefiles/rdbfmsua_orion.intel.lua
+++ modulefiles/rdbfmsua_orion.intel.lua
@@ -8,7 +8,7 @@ prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-
 local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.1.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
 
-load(pathJoin("stack-oneapi", stack_intel_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
 local bufr_ver=os.getenv("bufr_ver") or "12.1.0"
