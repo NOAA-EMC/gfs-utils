803d9e4298166363ea4de86625cccc68457caf92
Wei Huang <Wei.Huang@noaa.gov>
2025-08-20 14:42:14 +0000

    update to ss192

--- modulefiles/gfsutils_container.intel.lua
+++ modulefiles/gfsutils_container.intel.lua
@@ -2,9 +2,7 @@ help([[
 Build environment for GFS utilities in a container 
 ]])
 
-prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.1/envs/unified-env/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.1/envs/unified-env/install/modulefiles/intel-oneapi-mpi/2021.13-argr3sd/oneapi/2024.2.0")
-prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.1/envs/unified-env/install/modulefiles/oneapi/2024.2.0")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/Core")
 
 stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.0"
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
