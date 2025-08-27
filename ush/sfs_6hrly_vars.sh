e7287c3f6a9811573b38a0d5f10298ef2929ece6
KarinaAsmar-NOAA <148993962+KarinaAsmar-NOAA@users.noreply.github.com>
2025-08-27 15:48:57 -0400

    add {}
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>

--- ush/sfs_6hrly_vars.sh
+++ ush/sfs_6hrly_vars.sh
@@ -38,7 +38,7 @@ for (( i=0; i<${#vars[@]}; i++)); do
  
   cat $(eval "ls -v ${MEMDIR}/*") | wgrib2 - -match "${var}" -grib "${OUTDIR}/IN.grb"
 
-  wgrib2 "${OUTDIR}/IN.grb" -new_grid_winds earth -new_grid latlon 0:360:1 90:181:-1 "${OUTDIR}/$filename"
+  wgrib2 "${OUTDIR}/IN.grb" -new_grid_winds earth -new_grid latlon 0:360:1 90:181:-1 "${OUTDIR}/${filename}"
   rm "${OUTDIR}/IN.grb"
 
 done
