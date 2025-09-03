310f21b2b03f35e71c4ebe7640864a756301dfab
KarinaAsmar-NOAA <148993962+KarinaAsmar-NOAA@users.noreply.github.com>
2025-09-03 12:52:13 -0400

    add {} to GMERGE
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>

--- ush/sfs_atmos_daily_monthly.sh
+++ ush/sfs_atmos_daily_monthly.sh
@@ -185,7 +185,7 @@ do
     end_hr=$((j+24-6)) 
     list_6hrly=$(seq -f "${MEMDIR}/sfs.t00z.master.grb2f%03.0f" $start_hr 6 $end_hr)
     # shellcheck disable=SC2086
-    $GMERGE - ${list_6hrly} | wgrib2 - -match  "MSLET|PRMSL|PWAT|PRES:surface|TMP:2 m above|:TMP:surface|SPFH:2 m above|DPT:2 m above|UGRD:10 m above|VGRD:10 m above|HGT:(2|10|50|100|200|500|700|850|1000) mb|(PVORT|TMP):(450|550|650) K|(UGRD|VGRD):(2|10|50|100|200|500|600|700|850|925|1000) mb|SPFH:(100|200|300|500|600|700|850|925|1000) mb|VVEL:500 mb|TMP:(2|10|50|100|200|250|300|500|600|700|850|925|1000) mb|TOZNE|ICEC|ICETK|(TSOIL|SOILW):(0-0.1|0.1-0.4|0.4-1|1-2) m|WEASD|PEVPR|LAND|HGT:surface|CSDLF:surface|CSDSF:surface|CSUSF:surface|NDDSF:surface|VDDSF:surface|SOILM:0-0.2|TMP:1 hybrid" -fcst_ave 6hr "${OUTDIR}/inst.daily.${ENS}/daily_${end_hr}.grb"
+    ${GMERGE} - ${list_6hrly} | wgrib2 - -match  "MSLET|PRMSL|PWAT|PRES:surface|TMP:2 m above|:TMP:surface|SPFH:2 m above|DPT:2 m above|UGRD:10 m above|VGRD:10 m above|HGT:(2|10|50|100|200|500|700|850|1000) mb|(PVORT|TMP):(450|550|650) K|(UGRD|VGRD):(2|10|50|100|200|500|600|700|850|925|1000) mb|SPFH:(100|200|300|500|600|700|850|925|1000) mb|VVEL:500 mb|TMP:(2|10|50|100|200|250|300|500|600|700|850|925|1000) mb|TOZNE|ICEC|ICETK|(TSOIL|SOILW):(0-0.1|0.1-0.4|0.4-1|1-2) m|WEASD|PEVPR|LAND|HGT:surface|CSDLF:surface|CSDSF:surface|CSUSF:surface|NDDSF:surface|VDDSF:surface|SOILM:0-0.2|TMP:1 hybrid" -fcst_ave 6hr "${OUTDIR}/inst.daily.${ENS}/daily_${end_hr}.grb"
   done
 
   list_daily=$(ls -v "${OUTDIR}"/inst.daily."${ENS}"/daily_*.grb)
