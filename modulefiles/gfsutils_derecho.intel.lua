ae815f9b314e486557da44e5a7ec31d514be1c85
Wei Huang <Wei.Huang@noaa.gov>
2025-10-29 11:27:18 -0600

    on derecho

--- modulefiles/gfsutils_derecho.intel.lua
+++ modulefiles/gfsutils_derecho.intel.lua
@@ -7,10 +7,8 @@ prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/modulefiles_
 prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
 local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
-local stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or "2021.13"
-
 load(pathJoin("stack-oneapi", stack_oneapi_ver))
-load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
+
 
 load("gfsutils_common")
 
