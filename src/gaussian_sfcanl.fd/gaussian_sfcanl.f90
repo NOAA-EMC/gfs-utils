02a16f655ab36a55980305ee5bfe7e2e960763af
Michael Barlage <michael.barlage@noaa.gov>
2025-11-05 10:29:34 -0700

    Fix ice temperatures in sfcanl file when running with CICE (#159)
    # Description
    
    When creating sfcanl files in the GFSv17 framework with CICE, the fields
    tsoil1/2/3/4 get populated with constant values of 200K or 271.2K over
    ice grids. This results in bad initialization when used in downstream
    applications like GEFS. The replacement should only be done when ice is
    present and these constant values exist. The use of these constant
    values as a check also preserves functionality when not using CICE.
    
    Resolves #158

--- src/gaussian_sfcanl.fd/gaussian_sfcanl.f90
+++ src/gaussian_sfcanl.fd/gaussian_sfcanl.f90
@@ -363,6 +363,30 @@
    enddo
  enddo
 
+! When UFS uses the CICE seaice component, the ice temperatures (stored in the
+!  stc variable) are set to constant values (either 200K or 271.2K). This causes
+!  issues for downstream models using these ice temperatures.
+! The following block assigns an interpolated temperature calculated from CICE
+!  ice surface temperature and the fixed ice lower boundary temperature (271.2) 
+!  to grids with the following:
+!    - ice thickness > 0 or land mask /= 1  (not equal to land)
+!    - constant temperature of 200K or 271.2K in both ice layers
+
+ do i = 1, igaus*jgaus
+   if((gaussian_data%hice(i)  > 0.0 .or. gaussian_data%slmask(i) /= 1.0)          .and. &
+      ((gaussian_data%stc(i,1) > 199.999 .and. gaussian_data%stc(i,1) < 200.001)  .or.  &
+       (gaussian_data%stc(i,1) > 271.199 .and. gaussian_data%stc(i,1) < 271.201)) .and. &
+      gaussian_data%stc(i,1) == gaussian_data%stc(i,2)) then
+
+     gaussian_data%tsea(i)  = min(gaussian_data%tisfc(i),273.15)
+     gaussian_data%stc(i,1) = 0.75 * gaussian_data%tsea(i) + 0.25 * 271.2
+     gaussian_data%stc(i,2) = 0.25 * gaussian_data%tsea(i) + 0.75 * 271.2
+     gaussian_data%stc(i,3) = gaussian_data%stc(i,2)
+     gaussian_data%stc(i,4) = gaussian_data%stc(i,2)
+
+   end if
+ end do
+
  deallocate(col, row, s)
 
  deallocate(tile_data%orog)
