17c9b2cf4f0d1f2864ab9398be5c80991249ae6b
BoCui-NOAA <53531984+BoCui-NOAA@users.noreply.github.com>
2026-06-10 08:41:13 -0400

    Apply suggestion from @DavidHuber-NOAA
    Co-authored-by: David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>

--- src/gfs_bufr.fd/meteorg.f
+++ src/gfs_bufr.fd/meteorg.f
@@ -481,7 +481,6 @@
       if (fformat == 'netcdf') then
         error = nf90_get_att(ncid, nf90_global, "landsfcmdl",
      +  land_model_flag)
-        if (error /= 0) print*,'landsfcmdl not found'
         if (error /= 0) then
           print*,'landsfcmdl not found'
           land_model_flag = 0
